93f1aa2efcef17b947e82a9da11270b8c252b6d3
skniyamatullahofficial <skniyamatullahofficial@gmail.com>
2026-06-20 12:10:52 +0530

    Add verify all products displayed test case

--- src/test/java/com/saucedemo/constants/PageConstants.java
+++ src/test/java/com/saucedemo/constants/PageConstants.java
@@ -1,4 +1,5 @@
 package com.saucedemo.constants;
 
 public class PageConstants {
+    public static final int INVENTORY_PRODUCTS_COUNT = 6;
 }
--- src/test/java/com/saucedemo/pages/InventoryPage.java
+++ src/test/java/com/saucedemo/pages/InventoryPage.java
@@ -14,6 +14,7 @@ public class InventoryPage {
     //Locators
     By addToCartButton = By.id("add-to-cart-sauce-labs-backpack");
     By cartIcon = By.className("shopping_cart_badge");
+    By productItems = By.className("inventory_item");
 
     //Associate Methods
     public void clickOnAddToCartButton () {
@@ -23,5 +24,12 @@ public class InventoryPage {
     public String cartIconText (){
        return driver.findElement(cartIcon).getText();
         }
+
+    public int getAllProductCount() {
+        return driver.findElements(productItems).size();
+
+    }
+
+
 
     }
